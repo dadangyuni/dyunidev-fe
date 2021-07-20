025b4bb1d11cbf095e8012a89726cb9c4ef0fcfd
Dadang Yuni <dadanginuy@gmail.com>
2021-07-20 20:46:30 +0700

    Dadang

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dadang Yuni Utomo, Learn React JS Dadang
+          Dadang Yuni Utomo, Learn React JS Dadang Dadang
         </a>
       </header>
     </div>
