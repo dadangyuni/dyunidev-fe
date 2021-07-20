121c89b04aebea7551251b4f6658a9f15a19953a
Dadang Yuni <dadanginuy@gmail.com>
2021-07-20 16:52:40 +0700

    ST

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dadang Yuni Utomo
+          Dadang Yuni Utomo, ST
         </a>
       </header>
     </div>
